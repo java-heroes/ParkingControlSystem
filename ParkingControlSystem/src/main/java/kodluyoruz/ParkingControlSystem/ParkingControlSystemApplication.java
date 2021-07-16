229aba07a9c01b99270c277290c052bc3dbafc4b
sedanurgulocak <sedanurgulocak@gmail.com>
2021-07-16 16:58:53 +0300

    First commit

---
+++ ParkingControlSystem/src/main/java/kodluyoruz/ParkingControlSystem/ParkingControlSystemApplication.java
@@ -0,0 +1,13 @@
+package kodluyoruz.ParkingControlSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingControlSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingControlSystemApplication.class, args);
+	}
+
+}
---
+++ ParkingControlSystem/src/test/java/kodluyoruz/ParkingControlSystem/ParkingControlSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package kodluyoruz.ParkingControlSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingControlSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
