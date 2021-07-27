08461ed892174476942225c00c3d020ab7cf116a
sedanurgulocak <sedanurgulocak@gmail.com>
2021-07-28 00:48:31 +0300

    🔨Add field ParkLayout and associate ParkLayout with CarPark many to one

--- ParkingControlSystem/src/main/java/kodluyoruz/ParkingControlSystem/entities/concretes/ParkLayout.java
+++ ParkingControlSystem/src/main/java/kodluyoruz/ParkingControlSystem/entities/concretes/ParkLayout.java
@@ -1,5 +1,37 @@
 package kodluyoruz.ParkingControlSystem.entities.concretes;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="park_layouts")
+@Entity
 public class ParkLayout {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	//@Column(name="car_park_id")
+	//private int carParkId;
+	
+	@Column(name="name")
+	private char name;
+	
+	@ManyToOne()
+	@JoinColumn(name="car_park_id")
+	private CarPark carPark;
 }
