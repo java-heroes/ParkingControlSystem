168766a78a88513ac2ebe254056b718b2ded755b
sedanurgulocak <sedanurgulocak@gmail.com>
2021-07-28 20:44:29 +0300

    🔨Add park layout data access methods

--- ParkingControlSystem/src/main/java/kodluyoruz/ParkingControlSystem/dataAccess/abstracts/ParkLayoutDao.java
+++ ParkingControlSystem/src/main/java/kodluyoruz/ParkingControlSystem/dataAccess/abstracts/ParkLayoutDao.java
@@ -1,9 +1,26 @@
 package kodluyoruz.ParkingControlSystem.dataAccess.abstracts;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import kodluyoruz.ParkingControlSystem.entities.concretes.ParkLayout;
 
 public interface ParkLayoutDao extends JpaRepository<ParkLayout, Integer>{
 
+	ParkLayout getByName(char name);
+	
+	ParkLayout getByNameAndCarParkId(char name, int carParkId);
+	
+	List<ParkLayout> getByNameOrCarPark(char name, int carParkId);
+	
+	List<ParkLayout> getByCarParkIdIn(List<Integer> carParks);
+	
+	List<ParkLayout> getByNameContains(char name);
+	
+	List<ParkLayout> getByNameStartsWith(char name);
+	
+	@Query("From ParkLayout where name=:name and carPark.id=:carParkId")
+	List<ParkLayout> getByNameAndCarPark(char name, int carParkId);
 }
