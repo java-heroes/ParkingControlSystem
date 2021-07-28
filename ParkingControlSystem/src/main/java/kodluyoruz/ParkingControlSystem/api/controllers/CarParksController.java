7c9b7e77774e01d39bb52c73c6d885e8ab5d6c69
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-07-29 02:37:05 +0300

    🔨Add car park controller

---
+++ ParkingControlSystem/src/main/java/kodluyoruz/ParkingControlSystem/api/controllers/CarParksController.java
@@ -0,0 +1,45 @@
+package kodluyoruz.ParkingControlSystem.api.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import kodluyoruz.ParkingControlSystem.business.abstracts.CarParkService;
+import kodluyoruz.ParkingControlSystem.core.utilities.results.DataResult;
+import kodluyoruz.ParkingControlSystem.core.utilities.results.Result;
+import kodluyoruz.ParkingControlSystem.entities.concretes.CarPark;
+
+@RestController
+@RequestMapping("/api/carParks")
+public class CarParksController {
+	
+private CarParkService carParkService;
+	
+	@Autowired
+	public CarParksController(CarParkService carParkService) {
+		super();
+		this.carParkService = carParkService;
+	}
+
+	@GetMapping("/getall")
+	public  DataResult<List<CarPark>> getAll(){
+		return this.carParkService.getAll();
+	}
+
+	@PostMapping("/add")
+	public Result add(@RequestBody CarPark carPark) {
+		return this.carParkService.add(carPark);
+	}
+	
+	@GetMapping("/getByName")
+	public DataResult<CarPark> getByName(@RequestParam String name){
+		return this.carParkService.getByName(name);
+	}
+
+}
