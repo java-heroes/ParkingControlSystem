0334efc041e25b2a1b0f4fdb8006bb0cae4860a1
sedanurgulocak <sedanurgulocak@gmail.com>
2021-07-28 21:24:46 +0300

    🔨Add park layout service operations

--- ParkingControlSystem/src/main/java/kodluyoruz/ParkingControlSystem/business/abstracts/ParkLayoutService.java
+++ ParkingControlSystem/src/main/java/kodluyoruz/ParkingControlSystem/business/abstracts/ParkLayoutService.java
@@ -1,5 +1,29 @@
 package kodluyoruz.ParkingControlSystem.business.abstracts;
 
+import java.util.List;
+
+import kodluyoruz.ParkingControlSystem.core.utilities.results.DataResult;
+import kodluyoruz.ParkingControlSystem.core.utilities.results.Result;
+import kodluyoruz.ParkingControlSystem.entities.concretes.ParkLayout;
+
 public interface ParkLayoutService {
+	
+	DataResult<List<ParkLayout>> getAll();
+	
+	Result add(ParkLayout parkLayout);
+	
+	DataResult<ParkLayout> getByName(char name);
+	
+	DataResult<ParkLayout> getByNameAndCarParkId(char name, int carParkId);
+	
+	DataResult<List<ParkLayout>> getByNameOrCarPark(char name, int carParkId);
+	
+	DataResult<List<ParkLayout>> getByCarParkIdIn(List<Integer> carParks);
+	
+	DataResult<List<ParkLayout>> getByNameContains(char name);
+	
+	DataResult<List<ParkLayout>> getByNameStartsWith(char name);
+	
+	DataResult<List<ParkLayout>> getByNameAndCarPark(char name, int carParkId);
 
 }
