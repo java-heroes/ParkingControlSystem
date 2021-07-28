8274deb6609f4121fc0effaff9cdc0b146d219d3
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-07-28 21:34:26 +0300

    🔨Add park layout controller

--- ParkingControlSystem/src/main/java/kodluyoruz/ParkingControlSystem/api/controllers/ParkLayoutsController.java
+++ ParkingControlSystem/src/main/java/kodluyoruz/ParkingControlSystem/api/controllers/ParkLayoutsController.java
@@ -1,5 +1,46 @@
 package kodluyoruz.ParkingControlSystem.api.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import kodluyoruz.ParkingControlSystem.business.abstracts.ParkLayoutService;
+import kodluyoruz.ParkingControlSystem.core.utilities.results.DataResult;
+import kodluyoruz.ParkingControlSystem.core.utilities.results.Result;
+import kodluyoruz.ParkingControlSystem.entities.concretes.ParkLayout;
+
+@RestController
+@RequestMapping("/api/parkLayouts")
 public class ParkLayoutsController {
+	
+	private ParkLayoutService parkLayoutService;
+
+	@Autowired
+	public ParkLayoutsController(ParkLayoutService parkLayoutService) {
+		super();
+		this.parkLayoutService = parkLayoutService;
+	}
+	
+	@GetMapping("/getall")
+	public  DataResult<List<ParkLayout>> getAll(){
+		return this.parkLayoutService.getAll();
+	}
+
+	@PostMapping("/add")
+	public Result add(@RequestBody ParkLayout parkLayout) {
+		return this.parkLayoutService.add(parkLayout);
+	}
+	
+	@GetMapping("/getByName")
+	public DataResult<ParkLayout> getByName(@RequestParam char name){
+		return this.parkLayoutService.getByName(name);
+	}
 
 }
