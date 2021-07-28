1160c5c97e1993e92c6a473e015c8232c2704d7f
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-07-28 21:33:40 +0300

    🔨Add park layout manager

--- ParkingControlSystem/src/main/java/kodluyoruz/ParkingControlSystem/business/concretes/ParkLayoutManager.java
+++ ParkingControlSystem/src/main/java/kodluyoruz/ParkingControlSystem/business/concretes/ParkLayoutManager.java
@@ -1,5 +1,81 @@
 package kodluyoruz.ParkingControlSystem.business.concretes;
 
-public class ParkLayoutManager {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kodluyoruz.ParkingControlSystem.business.abstracts.ParkLayoutService;
+import kodluyoruz.ParkingControlSystem.core.utilities.results.DataResult;
+import kodluyoruz.ParkingControlSystem.core.utilities.results.Result;
+import kodluyoruz.ParkingControlSystem.core.utilities.results.SuccessDataResult;
+import kodluyoruz.ParkingControlSystem.core.utilities.results.SuccessResult;
+import kodluyoruz.ParkingControlSystem.dataAccess.abstracts.ParkLayoutDao;
+import kodluyoruz.ParkingControlSystem.entities.concretes.ParkLayout;
+
+@Service
+public class ParkLayoutManager implements ParkLayoutService{
+	
+	private ParkLayoutDao parkLayoutDao;
+	
+	@Autowired
+	public ParkLayoutManager(ParkLayoutDao parkLayoutDao) {
+		super();
+		this.parkLayoutDao = parkLayoutDao;
+	}
+
+	@Override
+	public DataResult<List<ParkLayout>> getAll() {
+		return new SuccessDataResult<List<ParkLayout>>(this.parkLayoutDao.findAll(), "Data listelendi");
+		
+	}
+
+	@Override
+	public Result add(ParkLayout parkLayout) {
+		this.parkLayoutDao.save(parkLayout);
+		return new SuccessResult("Otopark düzeni eklendi");
+	}
+
+	@Override
+	public DataResult<ParkLayout> getByName(char name) {
+		return new SuccessDataResult<ParkLayout>(this.parkLayoutDao.getByName(name), "Data listelendi");
+		
+	}
+
+	@Override
+	public DataResult<ParkLayout> getByNameAndCarParkId(char name, int carParkId) {
+		return new SuccessDataResult<ParkLayout>(this.parkLayoutDao.getByNameAndCarParkId(name, carParkId), "Data listelendi");
+		
+	}
+
+	@Override
+	public DataResult<List<ParkLayout>> getByNameOrCarPark(char name, int carParkId) {
+		return new SuccessDataResult<List<ParkLayout>>(this.parkLayoutDao.getByNameOrCarPark(name, carParkId), "Data listelendi");
+		
+	}
+
+	@Override
+	public DataResult<List<ParkLayout>> getByCarParkIdIn(List<Integer> carParks) {
+		return new SuccessDataResult<List<ParkLayout>>(this.parkLayoutDao.getByCarParkIdIn(carParks), "Data listelendi");
+		
+	}
+
+	@Override
+	public DataResult<List<ParkLayout>> getByNameContains(char name) {
+		return new SuccessDataResult<List<ParkLayout>>(this.parkLayoutDao.getByNameContains(name), "Data listelendi");
+		
+	}
+
+	@Override
+	public DataResult<List<ParkLayout>> getByNameStartsWith(char name) {
+		return new SuccessDataResult<List<ParkLayout>>(this.parkLayoutDao.getByNameStartsWith(name), "Data listelendi");
+		
+	}
+
+	@Override
+	public DataResult<List<ParkLayout>> getByNameAndCarPark(char name, int carParkId) {
+		return new SuccessDataResult<List<ParkLayout>>(this.parkLayoutDao.getByNameAndCarPark(name, carParkId), "Data listelendi");
+		
+	}
 
 }
