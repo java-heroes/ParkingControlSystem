5c8affeb05b5db08d5b80a3be01a0fe15fe4e1ac
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-07-29 02:24:41 +0300

    🔨Add car park service interface operations

--- ParkingControlSystem/src/main/java/kodluyoruz/ParkingControlSystem/business/abstracts/CarParkService.java
+++ ParkingControlSystem/src/main/java/kodluyoruz/ParkingControlSystem/business/abstracts/CarParkService.java
@@ -1,5 +1,29 @@
 package kodluyoruz.ParkingControlSystem.business.abstracts;
 
+import java.util.List;
+
+import kodluyoruz.ParkingControlSystem.core.utilities.results.DataResult;
+import kodluyoruz.ParkingControlSystem.core.utilities.results.Result;
+import kodluyoruz.ParkingControlSystem.entities.concretes.CarPark;
+
 public interface CarParkService {
+	
+	DataResult<List<CarPark>> getAll();
+	
+	Result add(CarPark carPark);
+	
+	DataResult<CarPark> getByName (String name);
+	
+	DataResult<CarPark> getByNameAndOwnerId(String name, int ownerId);
+
+	DataResult<List<CarPark>> getByNameOrOwnerId(String name, int ownerId);
+	
+	DataResult<List<CarPark>> getByOwnerIdIn(List<Integer> owners);
+	
+	DataResult<List<CarPark>> getByNameContains(String name);
+	
+	DataResult<List<CarPark>> getByNameStartsWith(String name);
+	
+	DataResult<List<CarPark>> getByNameAndOwner(String name, int ownerId);
 
 }
