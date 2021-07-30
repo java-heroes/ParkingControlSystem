c317036429886f0f283f98e9214ba29c0efa654d
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-07-30 11:38:33 +0300

    ⬆️ Inject parking layout service and create layout auto in database when park is added🔨Implement getByCapacity method

--- ParkingControlSystem/src/main/java/kodluyoruz/ParkingControlSystem/business/concretes/CarParkManager.java
+++ ParkingControlSystem/src/main/java/kodluyoruz/ParkingControlSystem/business/concretes/CarParkManager.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import kodluyoruz.ParkingControlSystem.business.abstracts.CarParkService;
+import kodluyoruz.ParkingControlSystem.business.abstracts.ParkLayoutService;
 import kodluyoruz.ParkingControlSystem.core.utilities.results.DataResult;
 import kodluyoruz.ParkingControlSystem.core.utilities.results.ErrorResult;
 import kodluyoruz.ParkingControlSystem.core.utilities.results.Result;
@@ -19,13 +20,16 @@ import kodluyoruz.ParkingControlSystem.entities.concretes.CarPark;
 @Service
 public class CarParkManager implements CarParkService{
 
-private CarParkDao carParkDao;
+	private CarParkDao carParkDao;
 	
 	@Autowired
 	public CarParkManager(CarParkDao carParkDao) {
 		super();
 		this.carParkDao = carParkDao;
 	}
+	
+	@Autowired (required = false)
+	private ParkLayoutService parkLayoutService;
 
 	@Override
 	public DataResult<List<CarPark>> getAll() {
@@ -36,7 +40,10 @@ private CarParkDao carParkDao;
 
 	@Override
 	public Result add(CarPark carPark) {
-		this.carParkDao.save(carPark);
+
+		CarPark newCarPark = carParkDao.save(carPark);
+		parkLayoutService.addParkLayoutName(newCarPark.getId());
+		
 		return new SuccessResult("Otopark eklendi");
 	}
 
@@ -102,5 +109,11 @@ private CarParkDao carParkDao;
 	public DataResult<List<CarPark>> getAllSorted() {
 		Sort sort = Sort.by(Sort.Direction.ASC, "name");
 		return new SuccessDataResult<List<CarPark>>(this.carParkDao.findAll(sort), "Otoparklar listelendi");
+	}
+
+	@Override
+	public DataResult<Integer> getByCapacity(int id) {
+		int temp = this.carParkDao.getByCapacity(id);
+		return new SuccessDataResult<Integer>(temp, "Otopark kapasitesi getirildi");
 	}
 }
