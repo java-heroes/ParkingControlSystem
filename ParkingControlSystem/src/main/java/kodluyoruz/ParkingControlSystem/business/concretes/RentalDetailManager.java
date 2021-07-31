49de536cbe7190e824c96d267d1deeb4dc935b8d
Merve Erkuş <merverkus17@gmail.com>
2021-07-31 20:35:06 +0300

    🔨Inject park rental service, implements service methods and set database columns automatically

--- ParkingControlSystem/src/main/java/kodluyoruz/ParkingControlSystem/business/concretes/RentalDetailManager.java
+++ ParkingControlSystem/src/main/java/kodluyoruz/ParkingControlSystem/business/concretes/RentalDetailManager.java
@@ -1,18 +1,18 @@
 package kodluyoruz.ParkingControlSystem.business.concretes;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import kodluyoruz.ParkingControlSystem.business.abstracts.ParkRentalService;
 import kodluyoruz.ParkingControlSystem.business.abstracts.RentalDetailService;
 import kodluyoruz.ParkingControlSystem.core.utilities.results.DataResult;
-import kodluyoruz.ParkingControlSystem.core.utilities.results.ErrorResult;
 import kodluyoruz.ParkingControlSystem.core.utilities.results.Result;
 import kodluyoruz.ParkingControlSystem.core.utilities.results.SuccessDataResult;
 import kodluyoruz.ParkingControlSystem.core.utilities.results.SuccessResult;
 import kodluyoruz.ParkingControlSystem.dataAccess.abstracts.RentalDetailDao;
+import kodluyoruz.ParkingControlSystem.entities.concretes.ParkRental;
 import kodluyoruz.ParkingControlSystem.entities.concretes.RentalDetail;
 
 
@@ -25,32 +25,34 @@ public class RentalDetailManager implements RentalDetailService {
 		super();
 		this.rentalDetailDao = rentalDetailDao;
 	}
+	
+	@Autowired (required = false)
+	private ParkRentalService parkRentalService;
 
 	@Override
 	public DataResult<List<RentalDetail>> getAll() {
 		return new SuccessDataResult<List<RentalDetail>>(this.rentalDetailDao.findAll(), "Data listelendi");
 	}
-
+	
 	@Override
-	public Result add(RentalDetail rentalDetail) {
-		this.rentalDetailDao.save(rentalDetail);
-		return new SuccessResult("Data eklendi");
-	}
-
-	@Override
-	public Result update(RentalDetail rentalDetail) {
-		Optional<RentalDetail> getRentalDetail = rentalDetailDao.findById(rentalDetail.getId());
-		  if(!getRentalDetail.isPresent()) {
-		         return new ErrorResult("Data ID'si bulunamadı");
-		    }
-	    this.rentalDetailDao.save(rentalDetail);
-		return new SuccessResult("Data güncellendi");
+	public DataResult<RentalDetail> getByParkRentalId(Integer parkRentalId){
+		return new SuccessDataResult<RentalDetail>(this.rentalDetailDao.getByParkRentalId(parkRentalId), "Data listelendi");
 	}
 
 	@Override
 	public Result delete(Integer id) {
 		this.rentalDetailDao.deleteById(id);
 		return new SuccessResult("Data silindi");
+	}
+
+	@Override
+	public Result addRentalDetail(int parkRentalId, float totalPrice) {
+		ParkRental tempRental=this.parkRentalService.getById(parkRentalId);
+		RentalDetail tempDetail = new RentalDetail();
+		tempDetail.setParkRental(tempRental);
+		tempDetail.setTotalPrice(totalPrice);
+		rentalDetailDao.save(tempDetail);
+		return new SuccessResult("Data eklendi");
 	}
 
 }
