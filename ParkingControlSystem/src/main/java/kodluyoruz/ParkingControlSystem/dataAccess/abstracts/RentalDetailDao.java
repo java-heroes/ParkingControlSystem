13969b4ea93b290b76ddd74086499befd918c41f
Merve Erkuş <merverkus17@gmail.com>
2021-07-31 20:29:51 +0300

    🔨Add new method getByParkRentalId

--- ParkingControlSystem/src/main/java/kodluyoruz/ParkingControlSystem/dataAccess/abstracts/RentalDetailDao.java
+++ ParkingControlSystem/src/main/java/kodluyoruz/ParkingControlSystem/dataAccess/abstracts/RentalDetailDao.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import kodluyoruz.ParkingControlSystem.entities.concretes.RentalDetail;
 
 public interface RentalDetailDao extends JpaRepository<RentalDetail, Integer> {
-
+	RentalDetail getByParkRentalId(Integer parkRentalId);
 }
