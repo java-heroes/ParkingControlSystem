6a400363c7b1ce02a57096c8aca2c15f3ff3a069
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-08-05 13:35:41 +0300

    ✨Add new entity ParkRentalDto

---
+++ ParkingControlSystem/src/main/java/kodluyoruz/ParkingControlSystem/entities/dto/ParkRentalDto.java
@@ -0,0 +1,12 @@
+package kodluyoruz.ParkingControlSystem.entities.dto;
+
+import java.util.Date;
+
+public class ParkRentalDto {
+	
+	public Date rentDate;
+	public Date endDate;
+	public int userId;
+	public int parkLayoutId;
+
+}
