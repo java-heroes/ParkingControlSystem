c4ddb4738aab2b35340625dcef32f2730081c8cb
Eda Dener <81362322+edadener@users.noreply.github.com>
2021-08-05 20:04:02 +0300

    ✨ Add new entity ParkRentalDtoUpdate

---
+++ ParkingControlSystem/src/main/java/kodluyoruz/ParkingControlSystem/entities/dto/ParkRentalDtoUpdate.java
@@ -0,0 +1,13 @@
+package kodluyoruz.ParkingControlSystem.entities.dto; 
+ 
+import java.util.Date; 
+ 
+public class ParkRentalDtoUpdate { 
+  
+ public int id; 
+ public Date endDate; 
+ public Date rentDate; 
+ public int userId; 
+ public int parkLayoutId; 
+ 
+}
