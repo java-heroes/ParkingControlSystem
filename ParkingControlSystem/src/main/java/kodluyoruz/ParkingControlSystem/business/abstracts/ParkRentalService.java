6f0baa02d7c0c11e3ee2031917f2cd69cee649f2
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-08-05 13:39:49 +0300

    🔨Add new add method to ParkRental

--- ParkingControlSystem/src/main/java/kodluyoruz/ParkingControlSystem/business/abstracts/ParkRentalService.java
+++ ParkingControlSystem/src/main/java/kodluyoruz/ParkingControlSystem/business/abstracts/ParkRentalService.java
@@ -3,17 +3,20 @@ package kodluyoruz.ParkingControlSystem.business.abstracts;
 import kodluyoruz.ParkingControlSystem.core.utilities.results.DataResult;
 import kodluyoruz.ParkingControlSystem.core.utilities.results.Result;
 import kodluyoruz.ParkingControlSystem.entities.concretes.ParkRental;
+import kodluyoruz.ParkingControlSystem.entities.dto.ParkRentalDto;
+
 import java.util.List;
 
 public interface ParkRentalService {
 
     DataResult<List<ParkRental>> getAll();
 
-    Result add(ParkRental parkRental);
-
     Result update(ParkRental parkRental);
 
     DataResult<ParkRental> deleteById(int id);
     
     ParkRental getById(Integer parkRentalId);
+    
+    Result add(ParkRentalDto parkRentalDto);
+    
 }
